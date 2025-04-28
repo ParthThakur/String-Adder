2bdc16bb03789b947269d745cf25346657294dcc
ParthThakur <parththakur@gmail.com>
2025-04-28 16:39:38 +0530

    add cases to check empty string and a single number

--- spec/string_adder_spec.rb
+++ spec/string_adder_spec.rb
@@ -4,4 +4,12 @@ RSpec.describe "#string_adder" do
   it "Returns sum of numbers speprated by commas" do
     expect(string_adder("1,2,3")).to eq(6)
   end
+
+  it "Returns 0 for empty string" do
+    expect(string_adder("")).to eq(0)
+  end
+
+  it "Handles single number" do
+    expect(string_adder("5")).to eq(5)
+  end
 end
